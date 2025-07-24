70a9e484a3e31bb23e7eeaf6849781deb7901625
Alexander Abanshin <alexanderabanshin@Alexanders-Mac-mini.local>
2025-07-25 09:56:58 +1100

    HWTM-5: Create folder and file.

---
+++ HWTM/AudienceHelp/AudienceHelp.swift
@@ -0,0 +1,18 @@
+//
+//  AudienceHelp.swift
+//  HWTM
+//
+//  Created by Alexander Abanshin on 25.07.2025.
+//
+
+import SwiftUI
+
+struct AudienceHelp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AudienceHelp()
+}
