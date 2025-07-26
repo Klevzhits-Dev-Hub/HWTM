fab0bedc096a72352f219b4811258a2118d981d8
Igor Klevzhits <klevzhits94@icloud.com>
2025-07-26 20:35:33 +0300

    HWTM-13: Fix Coordinator

--- HWTM/HWTMApp.swift
+++ HWTM/HWTMApp.swift
@@ -9,23 +9,12 @@ import SwiftUI
 
 @main
 struct HWTMApp: App {
-    @StateObject var coordinator = NavigationCoordinator()
+    @StateObject private var coordinator = NavigationCoordinator()
+
     var body: some Scene {
-        NavigationStack(path: $coordinator.path) {
-            HomeVM(initialState: .notStarted)
+        WindowGroup {
+            ContentView()
                 .environmentObject(coordinator)
-                .navigationDestination(for: AppScreen.self) { screen in
-                    switch screen {
-                    case .home:
-//                        HomeVM(initialState: .notStarted)
-                    case .game:
-//                        GameContentView(viewModel: viewModel)
-                    case .levelList:
-//                        LevelListView()
-                    case .gameOver:
-//                        GameOverView()
-                    }
-                }
         }
     }
 }
