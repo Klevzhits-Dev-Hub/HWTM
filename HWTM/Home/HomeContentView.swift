899f072914781ea1455ad9622869aa0565ab2705
Igor Klevzhits <klevzhits94@icloud.com>
2025-07-26 23:46:44 +0300

    HWTM-13: Fix navigation on level list

--- HWTM/Home/HomeContentView.swift
+++ HWTM/Home/HomeContentView.swift
@@ -130,14 +130,7 @@ struct HomeContentView: View {
                 }
             }
         }
-    }
-    private func navigationButtonToGame(lable: String, type: SystemButtonStyle, viewModel: GameViewModel, state: AnswerState) -> some View {
-      SystemButton(label: lable, type: type, state: state) {
-      isGoingToGameScreen = true
-    }
-    .navigationDestination(isPresented: $isGoingToGameScreen) {
-      GameContentView(viewModel: viewModel)
-    }
+    
   }
 }
 
